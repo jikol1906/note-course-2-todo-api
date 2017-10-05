e6b16fa097527a152483546d3bcbdf782d51ed85
Boris Grunwald <borisgrunwald@gmail.com>
2017-10-05 09:59:18 +0200

    Hash user password before saving

---
+++ playground/hashing.js
@@ -0,0 +1,39 @@
+const {SHA256} = require('crypto-js');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs')
+
+var password = '123abc!';
+
+bcrypt.genSalt(10,(err,salt) => {
+  bcrypt.hash(password,salt,(err,hash) => {
+    console.log(hash);
+  })
+})
+
+var hash = '$2a$10$W/BazQPrcN.A14mH9cOpse3I5MHLJJa.LGEXGivkgH/GWxPo10W4O';
+
+bcrypt.compare('123abc!',hash,(err,res) => {
+  console.log(res);
+})
+
+
+
+
+// var data = {
+//   id: 10
+// };
+
+// var token = jwt.sign(data, '123abc');
+//
+// var decoded = jwt.verify(token,'123abc');
+//
+// console.log(decoded);
+
+
+
+
+// var message = 'I am user number 3';
+// var hash = SHA256(message).toString();
+//
+// console.log(`Message: ${message}`);
+// console.log(`Hash: ${hash}`);
