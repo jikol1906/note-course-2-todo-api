20cabf8a0bd29197256f83e24447b569e68dec99
Boris Grunwald <borisgrunwald@gmail.com>
2017-10-11 19:32:58 +0200

    associate users with todos

--- server/server.js
+++ server/server.js
@@ -15,10 +15,11 @@ var port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
-app.post('/todos', (req,res) => {
+app.post('/todos', authenticate, (req,res) => {
 
   var todo = new Todo({
-    text: req.body.text
+    text: req.body.text,
+    _creator: req.user._id
   });
 
   todo.save().then((doc) => {
@@ -30,15 +31,17 @@ app.post('/todos', (req,res) => {
 
 
 
-app.get('/todos',(req,res) => {
-  Todo.find().then((todos) => {
+app.get('/todos', authenticate, (req,res) => {
+  Todo.find({
+    _creator: req.user._id
+  }).then((todos) => {
     res.send({todos});
   }, (e) => {
     res.status(400).send(e);
   });
 });
 
-app.get('/todos/:id', (req,res) => {
+app.get('/todos/:id',authenticate, (req,res) => {
 
   var id = req.params.id;
 
@@ -46,7 +49,10 @@ app.get('/todos/:id', (req,res) => {
     return res.status(404).send();
   }
 
-  Todo.findById(id).then((todo) => {
+  Todo.findOne({
+    _id: id,
+    _creator: req.user._id
+  }).then((todo) => {
     if(!todo) {
       return res.status(404).send();
     }
@@ -57,7 +63,7 @@ app.get('/todos/:id', (req,res) => {
 
 })
 
-app.patch('/todos/:id', (req,res) => {
+app.patch('/todos/:id', authenticate, (req,res) => {
 
   var id = req.params.id;
   var body = _.pick(req.body, ['text', 'completed']);
@@ -73,7 +79,10 @@ app.patch('/todos/:id', (req,res) => {
     body.completedAt = null;
   }
 
-  Todo.findByIdAndUpdate(id, {$set:body}, {new:true}).then((todo) => {
+  Todo.findOneAndUpdate({
+    _id:id,
+    _creator: req.user._id
+  }, {$set:body}, {new:true}).then((todo) => {
     if(!todo) {
       return res.status(404).send();
     }
@@ -87,7 +96,7 @@ app.patch('/todos/:id', (req,res) => {
 })
 
 
-app.delete('/todos/:id', (req,res) => {
+app.delete('/todos/:id', authenticate, (req,res) => {
 
   var id = req.params.id;
 
@@ -95,7 +104,10 @@ app.delete('/todos/:id', (req,res) => {
     return res.status(404).send()
   }
 
-  Todo.findByIdAndRemove(id).then((todo) => {
+  Todo.findOneAndRemove({
+    _id:id,
+    _creator: req.user._id
+  }).then((todo) => {
 
     if(!todo) {
       return res.status(404).send();
@@ -115,7 +127,7 @@ app.post('/users', (req,res) => {
   var user = new User(body);
 
   user.save().then(() => {
-      
+    return user.generateAuthToken();
   }).then((token) => {
     res.header('x-auth', token).send(user);
   }).catch((e) => {
